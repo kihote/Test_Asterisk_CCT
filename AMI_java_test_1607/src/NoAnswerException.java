2fdf150102388a3c194185e2d5f5bc1f0ca558de
Nguyen Quang Tri <tetehoki.@gmail.com>
2009-07-21 17:01:22 +0700

    JAVA _TEST _TETE

--- AMI_java_test_1607/src/NoAnswerException.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by IntelliJ IDEA.
- * User: Administrator
- * Date: Jul 21, 2009
- * Time: 2:02:13 PM
- * To change this template use File | Settings | File Templates.
- */
-public class NoAnswerException extends Exception {
-}
